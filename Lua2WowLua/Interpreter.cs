b0311af842565496edfcb0d884a59c25455362bd
fschwiet <fschwiet@gmail.com>
2010-09-25 09:24:16 -0700

    initial project

---
+++ Lua2WowLua/Interpreter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lua2WowLua
+{
+    public class Interpreter
+    {
+        public class Result
+        {
+            public string Filename;
+            public bool IsModule;
+            public string ModuleName;
+            public IEnumerable<string> Requires;
+        }
+
+        static Regex ModuleRegex = new Regex(@"^\s*module\s*\(\s*('|"")(?<name>\w+)('|"")\s*,\s*package\s*\.\s*seeall\s*\)\s*$");
+        static Regex UnrecognizedModuleRegex = new Regex(@"^\s*module\s*\(");
+
+        public static Result FromStream(Stream input)
+        {
+            var result = new Result();
+
+            using(var reader = new StreamReader(input))
+            {
+                string nextLine;
+                var lineIndex = 0;
+
+                while((nextLine = reader.ReadLine()) != null)
+                {
+                    lineIndex++;
+
+                    var match = ModuleRegex.Match(nextLine);
+
+                    if (match.Success)
+                    {
+                        result.IsModule = true;
+                        result.ModuleName = match.Groups["name"].Value;
+                    }
+                    else
+                    {
+                        match = UnrecognizedModuleRegex.Match(nextLine);
+
+                        if (match.Success)
+                            throw new InvalidDataException("Found unhandled module declaration on line " + lineIndex + ".");
+                    }
+
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Wow2WowLuaTest/InterpreterTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Lua2WowLua;
+using NUnit.Framework;
+
+namespace Wow2WowLuaTest
+{
+    [TestFixture]
+    public class InterpreterTest
+    {
+        [Test]
+        public void can_interpret_lua_file_with_module()
+        {
+            Stream telescopeDotLua =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("Wow2WowLuaTest.SampleFiles.telescope.lua");
+             
+            var result = Interpreter.FromStream(telescopeDotLua);
+
+            Assert.That(result.IsModule, Is.True);
+            Assert.That(result.ModuleName, Is.EqualTo("telescope"));
+        }
+
+        [Test]
+        public void can_interupt_lua_file_with_requires()
+        {
+            Stream tscDotLua =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("Wow2WowLuaTest.SampleFiles.tsc");
+
+            var result = Interpreter.FromStream(tscDotLua);
+
+            Assert.That(result.IsModule, Is.False);
+            Console.WriteLine(result.ModuleName);
+        }
+
+        [Test]
+        public void warns_if_finds_unrecognized_module()
+        {
+            var contents = new MemoryStream(Encoding.UTF8.GetBytes(@"1
+2
+3
+module('foo', lol)
+"));
+
+            try
+            {
+                var result = Interpreter.FromStream(contents);
+
+            }
+            catch (Exception e)
+            {
+                Assert.That(e.Message, Is.StringContaining("line 4"));
+                return;
+            }
+
+            Assert.Fail("exception expected");
+        }
+    }
+}
