f9c7927c26a9a806d413198c709a7c17e0c39ad9
fschwiet <fschwiet@gmail.com>
2010-11-21 14:08:43 -0800

    install as module

--- default.ps1
+++ default.ps1
@@ -1,33 +1,24 @@
 
 task default -depends deploy
 
-task uninstall {
-	if (test-path .\build\Lua2WowLua.dll) {
-		& installutil /uninstall .\build\Lua2WowLua.dll
-	}
-}
-
 task build {
 	exec { & $msbuild Lua2WowLua.sln }
-}
 
-task tests -depends build {
-	exec { nunit-console .\Wow2WowLuaTest\bin\Debug\Wow2WowLuaTest.dll }
-}
-
-task deploy -depends tests, uninstall {
-	
 	if (test-path .\build\) {
 		rm .\build\ -recurse
 	}
 
-	mkdir .\build\
+	$null = mkdir .\build\
 	cp .\Lua2WowLua\bin\debug\* .\build\
-
-	exec { & installutil /install .\build\Lua2WowLua.dll }
 }
 
-task demo -depends deploy {
-	add-pssnapin Lua2WowLua
+task tests -depends build {
+
+	exec { nunit-console .\Wow2WowLuaTest\bin\Debug\Wow2WowLuaTest.dll }
+
+}
+
+task demo -depends tests {
+	import-module .\build\Lua2WowLua.dll
 	gi  .\Wow2WowLuaTest\Scenarios\SharedRequireModule\*.lua | convert-Lua2WowLua -Source (gi  .\Wow2WowLuaTest\Scenarios\SharedRequireModule\main.lua)
 }
